b0ddd9e15cb9377e9b1005464f1f15360cc3cf76
Rohit <rkumar8092378845@gmail.com>
2021-01-20 23:54:36 +0530

    -> layout for the HomeFragment.kt

--- app/src/main/java/com/rohitthebest/projectplanner/ui/MainActivity.kt
+++ app/src/main/java/com/rohitthebest/projectplanner/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.rohitthebest.projectplanner
+package com.rohitthebest.projectplanner.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rohitthebest.projectplanner.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
