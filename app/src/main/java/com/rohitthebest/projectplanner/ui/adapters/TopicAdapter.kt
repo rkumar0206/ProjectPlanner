1ee5d9bb09a7dfc941b1b73b464f5bc20ab3639a
Rohit <rkumar8092378845@gmail.com>
2021-01-21 14:05:51 +0530

    -> Made adapter for showing topics

---
+++ app/src/main/java/com/rohitthebest/projectplanner/ui/adapters/TopicAdapter.kt
@@ -0,0 +1,46 @@
+package com.rohitthebest.projectplanner.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.rohitthebest.projectplanner.databinding.AdapterTopicLayoutBinding
+import com.rohitthebest.projectplanner.db.entity.Topic
+
+class TopicAdapter : ListAdapter<Topic, TopicAdapter.TopicViewHolder>(DiffUtilCallback()) {
+
+    private var mListener: OnClickListener? = null
+
+    inner class TopicViewHolder(val binding: AdapterTopicLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+
+
+    }
+
+    class DiffUtilCallback : DiffUtil.ItemCallback<Topic>() {
+
+        override fun areItemsTheSame(oldItem: Topic, newItem: Topic): Boolean = oldItem.topicKey == newItem.topicKey
+
+        override fun areContentsTheSame(oldItem: Topic, newItem: Topic): Boolean = oldItem == newItem
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TopicViewHolder {
+
+        val binding = AdapterTopicLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return TopicViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TopicViewHolder, position: Int) {
+
+    }
+
+    interface OnClickListener {
+
+        fun onItemClick(topic: Topic)
+    }
+
+    fun setOnClickListener(listener: OnClickListener) {
+        mListener = listener
+    }
+}
