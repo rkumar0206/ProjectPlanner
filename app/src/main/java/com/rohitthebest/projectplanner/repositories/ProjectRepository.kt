3842727b7a529bf28cd90394ff4d36a6baca1ff1
Rohit <rkumar8092378845@gmail.com>
2021-01-21 12:10:02 +0530

    -> defined ProjectRepository.kt and ProjectViewModel.kt

---
+++ app/src/main/java/com/rohitthebest/projectplanner/repositories/ProjectRepository.kt
@@ -0,0 +1,22 @@
+package com.rohitthebest.projectplanner.repositories
+
+import com.rohitthebest.projectplanner.db.dao.ProjectDao
+import com.rohitthebest.projectplanner.db.entity.Project
+import javax.inject.Inject
+
+class ProjectRepository @Inject constructor(
+        var dao: ProjectDao
+) {
+
+    suspend fun insertProject(project: Project) = dao.insertProject(project)
+
+    suspend fun insertProjects(projects: List<Project>) = dao.insertProjects(projects)
+
+    suspend fun deleteProject(project: Project) = dao.deleteProject(project)
+
+    suspend fun deleteAllProjects() = dao.deleteAllProjects()
+
+    suspend fun updateProject(project: Project) = dao.updateProject(project)
+
+    fun getAllProjects() = dao.getAllProjects()
+}
---
+++ app/src/main/java/com/rohitthebest/projectplanner/ui/viewModels/ProjectViewModel.kt
@@ -0,0 +1,42 @@
+package com.rohitthebest.projectplanner.ui.viewModels
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.rohitthebest.projectplanner.db.entity.Project
+import com.rohitthebest.projectplanner.repositories.ProjectRepository
+import kotlinx.coroutines.launch
+
+class ProjectViewModel @ViewModelInject constructor(
+        val repository: ProjectRepository
+) : ViewModel() {
+
+    fun insertProject(project: Project) = viewModelScope.launch {
+
+        repository.insertProject(project)
+    }
+
+    fun insertProjectList(projects: List<Project>) = viewModelScope.launch {
+
+        repository.insertProjects(projects)
+    }
+
+    fun updateProject(project: Project) = viewModelScope.launch {
+
+        repository.updateProject(project)
+    }
+
+    fun deleteProject(project: Project) = viewModelScope.launch {
+
+        repository.deleteProject(project)
+    }
+
+    fun deleteAllProjects() = viewModelScope.launch {
+
+        repository.deleteAllProjects()
+    }
+
+    val projects = repository.getAllProjects().asLiveData()
+
+}
