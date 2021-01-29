e6ac5ae7efa35b11621142ee1dabba05f6dd5109
Rohit <rkumar8092378845@gmail.com>
2021-01-29 15:10:59 +0530

    -> Made adapter for Colors recyclerView ColorsAdapter.kt

---
+++ app/src/main/java/com/rohitthebest/projectplanner/ui/adapters/ColorsAdapter.kt
@@ -0,0 +1,56 @@
+package com.rohitthebest.projectplanner.ui.adapters
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.rohitthebest.projectplanner.databinding.AdapterColorsLayoutBinding
+import com.rohitthebest.projectplanner.db.entity.Colors
+
+class ColorsAdapter : ListAdapter<Colors, ColorsAdapter.ColorsViewHolder>(DiffUtilCallback()) {
+
+    private var mListener: OnClickListener? = null
+
+    inner class ColorsViewHolder(val binding: AdapterColorsLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun setData(color: Colors?) {
+
+            color?.let {
+
+                binding.colorNameTV.text = color.colorName
+                binding.colorHexCodeTV.text = color.colorHexCode
+                binding.viewToBeColored.setBackgroundColor(Color.parseColor(color.colorHexCode))
+            }
+        }
+    }
+
+    class DiffUtilCallback : DiffUtil.ItemCallback<Colors>() {
+
+        override fun areItemsTheSame(oldItem: Colors, newItem: Colors): Boolean = oldItem == newItem
+
+        override fun areContentsTheSame(oldItem: Colors, newItem: Colors): Boolean = oldItem == newItem
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ColorsViewHolder {
+
+        val binding = AdapterColorsLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ColorsViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ColorsViewHolder, position: Int) {
+
+        holder.setData(getItem(position))
+    }
+
+    interface OnClickListener {
+
+        fun onColorClicked(color: Colors)
+    }
+
+    fun setOnClickListener(listener: OnClickListener) {
+        mListener = listener
+    }
+}
