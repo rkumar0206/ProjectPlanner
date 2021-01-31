2cc6607fd9f24d4e7a46e60ac22497920c3ea41e
Rohit <rkumar8092378845@gmail.com>
2021-02-01 00:35:39 +0530

    -> Added TaskRepository.kt

---
+++ app/src/main/java/com/rohitthebest/projectplanner/repositories/TaskRepository.kt
@@ -0,0 +1,30 @@
+package com.rohitthebest.projectplanner.repositories
+
+import com.rohitthebest.projectplanner.db.dao.TaskDao
+import com.rohitthebest.projectplanner.db.entity.Task
+import javax.inject.Inject
+
+class TaskRepository @Inject constructor(
+        val dao: TaskDao
+) {
+
+    suspend fun insertTask(task: Task) = dao.insertTask(task)
+
+    suspend fun insertTasks(tasks: List<Task>) = dao.insertTasks(tasks)
+
+    suspend fun updateTask(task: Task) = dao.updateTask(task)
+
+    suspend fun deleteTask(task: Task) = dao.deleteTask(task)
+
+    suspend fun deleteAllTasks() = dao.deleteAllTasks()
+
+    suspend fun deleteTaskByIsCompleted(isCompleted: String) = dao.deleteTaskByIsCompleted(isCompleted)
+
+    suspend fun deleteTaskByProjectKey(projectKey: String) = dao.deleteTaskByProjectKey(projectKey)
+
+    fun getAllTasks() = dao.getAllTasks()
+
+    fun getAllTaskByProjectKey(projectKey: String) = dao.getAllTaskByProjectKey(projectKey)
+
+    fun getTaskCountByProjectKey(projectKey: String) = dao.getTaskCountByProjectKey(projectKey)
+}
