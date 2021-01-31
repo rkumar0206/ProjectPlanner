f5c6170b6b41702fc093b05a903137dba87e5ea7
Rohit <rkumar8092378845@gmail.com>
2021-02-01 00:42:22 +0530

    -> Added TaskViewModel.kt

---
+++ app/src/main/java/com/rohitthebest/projectplanner/ui/viewModels/TaskViewModel.kt
@@ -0,0 +1,54 @@
+package com.rohitthebest.projectplanner.ui.viewModels
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.rohitthebest.projectplanner.db.entity.Task
+import com.rohitthebest.projectplanner.repositories.TaskRepository
+import kotlinx.coroutines.launch
+
+class TaskViewModel @ViewModelInject constructor(
+        val repository: TaskRepository
+) : ViewModel() {
+
+    fun insertTask(task: Task) = viewModelScope.launch {
+
+        repository.insertTask(task)
+    }
+
+    fun insertTasks(tasks: List<Task>) = viewModelScope.launch {
+        repository.insertTasks(tasks)
+    }
+
+    fun updateTask(task: Task) = viewModelScope.launch {
+
+        repository.updateTask(task)
+    }
+
+    fun deleteTask(task: Task) = viewModelScope.launch {
+
+        repository.deleteTask(task)
+    }
+
+    fun deleteAllTasks() = viewModelScope.launch {
+
+        repository.deleteAllTasks()
+    }
+
+    fun deleteTaskByIsCompleted(isCompleted: String) = viewModelScope.launch {
+
+        repository.deleteTaskByIsCompleted(isCompleted)
+    }
+
+    fun deleteTaskByProjectKey(projectKey: String) = viewModelScope.launch {
+
+        repository.deleteTaskByProjectKey(projectKey)
+    }
+
+    var allTasks = repository.getAllTasks().asLiveData()
+
+    fun getAllTaskByProjectKey(projectKey: String) = repository.getAllTaskByProjectKey(projectKey).asLiveData()
+
+    fun getTaskCountByProjectKey(projectKey: String) = repository.getTaskCountByProjectKey(projectKey)
+}
