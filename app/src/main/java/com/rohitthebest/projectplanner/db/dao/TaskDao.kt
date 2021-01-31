13fa52d44b35377530272380682e255f1f4d8c90
Rohit <rkumar8092378845@gmail.com>
2021-02-01 00:26:51 +0530

    -> Added Task Dao

---
+++ app/src/main/java/com/rohitthebest/projectplanner/db/dao/TaskDao.kt
@@ -0,0 +1,41 @@
+package com.rohitthebest.projectplanner.db.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.rohitthebest.projectplanner.db.entity.Task
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TaskDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTask(task: Task)
+
+    @Insert
+    suspend fun insertTasks(tasks: List<Task>)
+
+    @Update
+    suspend fun updateTask(task: Task)
+
+    @Delete
+    suspend fun deleteTask(task: Task)
+
+    @Query("DELETE FROM task_table")
+    suspend fun deleteAllTasks()
+
+    @Query("DELETE FROM task_table WHERE isCompleted = :isCompleted")
+    suspend fun deleteTaskByIsCompleted(isCompleted: String)
+
+    @Query("DELETE FROM task_table WHERE projectKey= :projectKey")
+    suspend fun deleteTaskByProjectKey(projectKey: String)
+
+    @Query("SELECT * FROM task_table ORDER BY timeStamp DESC")
+    fun getAllTasks(): Flow<List<Task>>
+
+    @Query("SELECT * FROM task_table WHERE projectKey= :projectKey ORDER BY timeStamp DESC")
+    fun getAllTaskByProjectKey(projectKey: String): Flow<List<Task>>
+
+    @Query("SELECT COUNT(taskKey) FROM task_table WHERE projectKey= :projectKey")
+    fun getTaskCountByProjectKey(projectKey: String): LiveData<Int>
+
+}
